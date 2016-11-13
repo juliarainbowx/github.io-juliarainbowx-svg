638a4dee5411d2d1f65c6bfadaaa213f0fcf4468
Julia <jukostyuchenko@gmail.com>
2016-11-13 22:36:04 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
+$(document).ready(() => {
+  setBg();
+  $(".editor").draggable();
 
+  $("#editor").keyup(() => {
+    setBg();
+  });
+});
+
+function setBg() {
+  var plainSvg = $("#editor").val();
+  var escapedSvg = escapeHtml(plainSvg);
+  var result = "background-image: url(\"data:image/svg+xml,"+escapedSvg+"\");";
+
+  $("body").attr("style", result);
+}
+function escapeHtml(unsafe) {
+    return unsafe
+         .replace(/#/g, "%23")
+         .replace(/</g, "%3C")
+         .replace(/>/g, "%3E")
+         .replace(/"/g, "'")
+         .replace(/\s{2,}/g, " ")
+ }
